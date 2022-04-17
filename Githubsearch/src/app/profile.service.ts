f81008868b79d322ffeb9f2ffef715a7d658d873
FeverCode <giddylancs.gl@gmail.com>
2022-04-17 19:46:46 +0300

    Removed unused code to declutter

--- Githubsearch/src/app/profile.service.ts
+++ Githubsearch/src/app/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+
 
 
 @Injectable()
@@ -20,15 +20,14 @@ export class ProfileService {
     interface ApiResponse {
       login: string;
     }
-    // tslint:disable-next-line:max-line-length
     return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
-    //  .pipe(map(res => res.json()));
+     
   }
 
   getProfileRepos() {
-    // tslint:disable-next-line:max-line-length
+  
     return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
-      // .pipe(map(res => res.json()));
+      
   }
 
   updateProfile(username: string) {
