0f7ba7d6e5486384d47818e5a72203910723ab7d
FeverCode <giddylancs.gl@gmail.com>
2022-04-17 13:33:27 +0300

    Added Navbar and Routing

--- Githubsearch/src/app/app.module.ts
+++ Githubsearch/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
