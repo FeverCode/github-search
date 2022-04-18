feec4a628f56b3fedc442c9122767eefd19fbc75
FeverCode <giddylancs.gl@gmail.com>
2022-04-18 10:35:42 +0300

    resolved route module errors

--- Githubsearch/src/app/app-routing.module.ts
+++ Githubsearch/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProfileComponent } from './profile/profile.component';
 import { AboutComponent } from './about/about.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 
 const routes: Routes = [
